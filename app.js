57121aeee6481ec8951b27f694b203a38a308608
Viktoriia Hilenko <80678378+vixxxyg@users.noreply.github.com>
2024-12-12 18:24:53 +0100

    6th

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const pumpkin = document.querySelector('.site-header__img');
+const boo = document.querySelector('.site-header__main-header');
 pumpkin.addEventListener('click', playScarySound);
 
 function playScarySound() {
-    console.log('click');
+    const audio = new Audio();
+    audio.preload = "auto";
+    audio.src = "./audio/boo.mp3";
+    audio.play();
+    boo.classList.remove('hidden');
 }
